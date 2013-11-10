d1959fcc600f7e23df31c1c7207d742d11828c80
Matteo Giaccone <matteo.giaccone@welaika.com>
2013-11-10 17:48:39 +0100

    Project-Category relation editable

--- app/admin/project.rb
+++ app/admin/project.rb
@@ -1,8 +1,25 @@
 ActiveAdmin.register Project do
   controller do
     def permitted_params
-      params.permit project: [:title, :subtitle, :equipe, :description, :date]
+      params.permit project: [:title, :subtitle, :equipe, :description, :date, category_ids: []]
     end
+  end
+
+  form do |f|
+    f.inputs "Details" do
+      f.input :title
+      f.input :subtitle
+    end
+    f.inputs "Content" do
+      f.input :date
+      f.input :equipe
+      f.input :description
+    end
+
+    f.inputs "Categories" do
+      f.inputs :categories
+    end
+    f.actions
   end
 end
 
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ActiveRecord::Base
   has_and_belongs_to_many :categories
+  accepts_nested_attributes_for :categories
 end
 
