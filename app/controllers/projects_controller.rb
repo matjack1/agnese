8cd452e1043c9c335b6f77c66cbd56489aa0447a
Matteo Giaccone <matteo.giaccone@welaika.com>
2013-11-10 21:07:24 +0100

    Basic visualization of projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,13 @@
 class ProjectsController < ApplicationController
   def index
+    if params[:category]
+      @projects = Project.joins(:categories).where(categories: {id: params[:category]})
+    else
+      @projects = Project.all
+    end
   end
 
   def show
   end
 end
+
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def categories_navigation
+    content_tag(:ul) do
+      Category.all.map do |c|
+        content_tag(:li) do
+          link_to c.name, projects_path(category: c)
+        end
+      end.join.html_safe
+    end
+  end
 end
+
